f056b5056f85068b635fc4f6f2590870661ab6b6
23564 <707280427@qq.com>
2021-01-29 10:41:26 +0800

    fetch

--- src/api/home/index.js
+++ src/api/home/index.js
@@ -1,10 +1,10 @@
 import Fetch from '@/service/fetch'
-import Axios from '@/service/axios'
+//import Axios from '@/service/axios'
 
 // fetch获取首页列表信息
 export const getList = (params)=>  Fetch('/api/trade/support/v1/home/order/list',params)
 
-export const getData = (params)=>  Axios('/api/trade/support/v1/home/order/list',params)
+// export const getData = (params)=>  Axios('/api/trade/support/v1/home/order/list',params)
 
-      
+export const getData = (params)=>  Fetch('/api/trade/support/v1/home/order/list',params)     
 
