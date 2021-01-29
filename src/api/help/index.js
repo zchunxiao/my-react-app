3187899b2704049790239bf17e3098ce8a40dfa5
23564 <707280427@qq.com>
2021-01-29 13:08:10 +0800

    1

--- src/api/help/index.js
+++ src/api/help/index.js
@@ -1,11 +1,11 @@
-import Axios from '@/service/axios'
-
+//import Axios from '@/service/axios'
+import Fetch from '@/service/fetch'
 // 检查登录状态
 export function checkLoginState(data) {
-    return  Axios("/user-customer-web/api/login/checkLoginState", data);
+    return Fetch("/user-customer-web/api/login/checkLoginState", data);
 }
 
 // 乐高接口
 export function getLego(data){
-    return Axios(`/ruban/api/lego/pageInfo?pageId=113040`,data,{method:"get"},true)
+    return Fetch(`/ruban/api/lego/pageInfo?pageId=113040`,data,{method:"get"},true)
 }
