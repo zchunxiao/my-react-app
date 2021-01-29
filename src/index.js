ccf033f210a281541acea513fa4e49f3ef979631
23564 <707280427@qq.com>
2021-01-29 09:20:26 +0800

    1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import FastClick from 'fastclick';
-
+import 'antd-mobile/dist/antd-mobile.css';  
 // 绑定到body
 FastClick.attach(document.body);
 
--- src/views/wallet/Wallet.js
+++ src/views/wallet/Wallet.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import { WhiteSpace } from 'antd-mobile';
+import "./index.css"
 
+const PlaceHolder = ({ className = '', ...restProps }) => (
+    <div className={`${className} placeholder`} {...restProps}>Block</div>
+  );
 class Wallet extends Component {
     render() {
         return (
             <div className="help-box">
                 钱包 - 页面
+                <div>
+                    <WhiteSpace size="xs" />
+                    <PlaceHolder />
+
+                    <WhiteSpace size="sm" />
+                    <PlaceHolder />
+
+                    <WhiteSpace />
+                    <PlaceHolder />
+
+                    <WhiteSpace size="lg" />
+                    <PlaceHolder />
+
+                    <WhiteSpace size="xl" />
+                    <PlaceHolder />
+                </div>
             </div>
         );
     }
